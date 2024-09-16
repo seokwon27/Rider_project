bb326f42bb093f123124af982becce5628506497
seokwon <tjrtkd95@gmail.com>
2024-09-16 14:22:03 +0900

    init: 메인페이지 리스트 브랜치 생성

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,26 @@
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import styled from "styled-components";
+import SearchPanel from "./SearchPanel";
+import axios from "axios";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <StSection>
+      <SearchPanel />
+      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "100vh" }}>
+        {" "}
+        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+          <div style={{ color: "#000" }}>Hello World!</div>
+        </MapMarker>
+      </Map>
+    </StSection>
+  );
 };
+
+const StSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+`;
 
 export default Home;
