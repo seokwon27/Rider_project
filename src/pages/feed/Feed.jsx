033ae46b54dcf2fb014629549548aa6bcda22c1c
hadooni <hayoung7259@gmail.com>
2024-09-19 17:09:27 +0900

    fix: 코드수정

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -1,13 +1,20 @@
+import styled from "styled-components";
 import Posting from "./Posting";
 import TopButton from "./TopButton";
 
 const Feed = () => {
   return (
-    <div>
+    <FeedWrapper>
       <Posting />
       <TopButton />
-    </div>
+    </FeedWrapper>
   );
 };
 
 export default Feed;
+
+const FeedWrapper = styled.div`
+  background-color: #121212;
+  padding: 10px 0px 20px 0px;
+  min-height: 100vh;
+`;
