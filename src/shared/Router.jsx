71f8ac4d4133f85e9df674cb96d5f74ab812aeb5
escape-engineering <tkdrnr1215@naver.com>
2024-09-19 17:48:46 +0900

    feat: jsonserver 버전 변경에 따른 pageNum계산 방법 변경

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Mypage from "../pages/myPage/Mypage";
 import Feed from "../pages/feed/Feed";
+import Landing from "../pages/landing/Landing";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/mypage" element={<Mypage />}></Route>
         <Route path="/feed" element={<Feed />}></Route>
       </Routes>
