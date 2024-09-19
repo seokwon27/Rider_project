9ed9dee34bfe3636460913605a4492dd130af80b
kimseoyun98 <seoyunkim98@gmail.com>
2024-09-19 17:17:33 +0900

    login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,35 @@
-const Login = () => {
-  return <div>Login</div>;
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthForm from "../../components/AuthForm";
+import { getUserProfile, login } from "../../api/auth";
+
+const Login = ({ setUser }) => {
+  const navigate = useNavigate();
+  const handleLogin = async (formData) => {
+    try {
+      const loginData = await login(formData);
+      localStorage.setItem("accessToken", loginData.accessToken);
+
+      const userProfile = await getUserProfile(loginData.accessToken);
+      setUser(userProfile);
+
+      navigate("/");
+    } catch (error) {
+      alert("로그인에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
+  return (
+    <div className="w-full flex flex-col items-center justify-center mt-20 space-y-10">
+      <h1 className="text-3xl text-center">로그인</h1>
+      <AuthForm mode="login" onSubmit={handleLogin} />
+      <p className="text-sm">
+        계정이 없으신가요?{" "}
+        <Link to="/signup" className="text-gray-400 hover:text-blue-500 underline">
+          회원가입 바로가기
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Mypage from "../pages/myPage/Mypage";
+import ProtectedRoute from "../components/ProtectedRoute";
+import { useState } from "react";
+import Login from "../pages/auth/Login";
+import Signup from "../pages/auth/Signup";
+import Feed from "../pages/feed/Feed";
+import Layout from "../components/Layout";
 
 const Router = () => {
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/mypage" element={<Mypage />}></Route>
-      </Routes>
+      <Layout user={user} setUser={setUser}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          {/* <Route path="/signup" element={<Signup />} /> */}
+          <Route path="/feed" element={<Feed setUser={setUser} />} />
+
+          {/* <Route
+          path="/mypage"
+          element={
+            <ProtectedRoute user={user}>
+              <Mypage user={user} setUser={setUser} />
+            </ProtectedRoute>
+          }
+        /> */}
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
