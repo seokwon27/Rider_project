2ccf65c6137f437f2108cea93172079f1136526d
escape-engineering <tkdrnr1215@naver.com>
2024-09-22 02:42:23 +0900

    refactor: 좋아요 기능 로그인 제한

--- src/pages/feed/Thumb.jsx
+++ src/pages/feed/Thumb.jsx
@@ -4,20 +4,27 @@ import { faThumbsUp as solidThumb } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 import useUserStore from "../../store/useUserStore";
 import { useThumbMutation } from "../../queries/mutaions";
+import { useNavigate } from "react-router-dom";
 
 const Thumb = ({ currentFeedId, currentThumb }) => {
+  const navigate = useNavigate();
   const { user } = useUserStore();
   const { mutate: addThumb } = useThumbMutation();
 
   const isUserLiked = currentThumb.some((el) => el == user?.id);
   const thumbCount = currentThumb.length;
+  const handleThumb = () => {
+    if (!user) {
+      alert("로그인 후 가능합니다!");
+      navigate("/login");
+      return;
+    }
+    addThumb({ feedId: currentFeedId, currentThumb, isUserLiked, user });
+  };
 
   return (
     <div>
-      <Button
-        onClick={() => addThumb({ feedId: currentFeedId, currentThumb, isUserLiked, user })}
-        disabled={isUserLiked ? false : true}
-      >
+      <Button onClick={handleThumb} disabled={isUserLiked ? false : true}>
         {isUserLiked ? <FontAwesomeIcon icon={solidThumb} /> : <FontAwesomeIcon icon={regularThumb} />}
       </Button>
       {thumbCount}
