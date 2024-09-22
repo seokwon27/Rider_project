cf0b4be1a18422d59ae002a8643eaf885b4413f2
escape-engineering <tkdrnr1215@naver.com>
2024-09-22 22:53:57 +0900

    feat: infiniteQuery suspense관련 코드 추가 및 로딩시 스피너 컴포넌트 fallback추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import "./index.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+import React, { Suspense } from "react";
+import GlobalLoading from "./components/GlobalLoading.jsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <App />
-    {/* <ReactQueryDevtools /> */}
+    <Suspense fallback={<GlobalLoading />}>
+      <App />
+    </Suspense>
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
